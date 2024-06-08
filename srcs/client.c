81e11862afba1b4f66695e858ef568b3f2e5258b
paulorsfaria <paulofaria.esaf@gmail.com>
2024-06-08 01:23:44 +0100

    not even close

--- srcs/client.c
+++ srcs/client.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 
 static unsigned char bit = 0b00000000; // Initialize bit to 0
 
-void ft_send_bits(int i, int b)
+void ft_print_bits(int i, int b)
 {
-	// Set or clear the bit at position i
 	if (b == 1)
 		bit |= (1 << i); // mete a 1
 	else
@@ -13,26 +14,29 @@ void ft_send_bits(int i, int b)
 		printf("c = %c\n", bit);
 }
 
-void ft_print_bits(unsigned char bitter)
+
+void ft_send_bits(unsigned char bitter)
 {
 	int i = 8;
 	while (i--)
 	{
 		if (bitter >> i & 1)
-			ft_send_bits(i, 1);
+			ft_print_bits(i, 1);
 		else
-			ft_send_bits(i, 0);
+			ft_print_bits(i, 0);
 	}
 }
 
+
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		printf("Usage: %s <character>\n", argv[0]);
+	if (argc != 2)
 		return 1;
-	}
 
 	printf("\n");
-	ft_print_bits(argv[1][0]);
+	ft_send_bits(argv[1][0]);	ft_send_bits(argv[1][1]);
+	while (1)
+		printf("%d\n", getpid());
+
 	return 0;
 }
